b55d99ea405e46843dab219705d15783358deb9b
Indoors-Cat <43878134+Indoors-Cat@users.noreply.github.com>
2024-12-15 20:11:10 -0500

    Set default volume at 100% (commented out by default) & added picom experimental backend option

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-nitrogen --restore &
-picom &
 lxsession &
+nitrogen --restore &
 nm-applet &
+picom &
+#pactl set-sink-volume @DEFAULT_SINK@ 100% &
 #slstatus &
 #ckb-next &
+#nm-applet &
 #/usr/bin/emacs --daemon &
+#picom --experimental-backend &  #This is being used with picom-jonaburg-git
